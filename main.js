a7f83f5aa7b3e32bd8690598281c557af6427160
abduvali0626 <umarrrrrrrrrrrrrrr63@gmail.com>
2025-02-06 19:41:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let body = document.getElementById("body");
+
+function almashtir1() {
+  body.style.backgroundImage = "url(venus-atmosphere.jpg)";
+}
+
+
+
+console.log(90 + 100);
+
+
+function almashtir2() {
+  body.style.backgroundImage =
+    "url(latest-image-from-the-surface-of-mars-on-january-15-2024-v0-44etcn4g9tcc1.webp)";
+}
